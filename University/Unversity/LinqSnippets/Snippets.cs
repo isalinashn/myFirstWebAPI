422d288932cf0afd56795d1c9c6590bf17477f1f
Israel Salinas <98296770+isalinashn@users.noreply.github.com>
2023-04-29 10:21:55 -0600

    Ejercicio 03
    Agregadas los ejercicios de las funciones usando LinQ

---
+++ University/Unversity/LinqSnippets/Snippets.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace LinqSnippets
+{
+    public class Snippets
+    {
+        static public void BasicLinQ()
+        {
+            string[] cars =
+            {
+                "VW Golf",
+                "VW California",
+                "Audi A3",
+                "Audi A4",
+                "Fiat Punto",
+                "Seat Ibiza",
+                "Seat León"
+            };
+
+            // 1. Select * of cars
+            var carList = from car in cars select car;
+            foreach ( var car in carList )
+            {
+                Console.WriteLine(car);
+            }
+
+            var audiList = from car in cars where car.Contains("Audi") select car;
+            foreach ( var audi in audiList )
+            {
+                Console.WriteLine(audi);
+            }
+
+        }
+
+        // Number examples
+        static public void LinqNumbers()
+        {
+            List<int> numbers = new List<int> { 1,2,3,4,5,6,7,8,9};
+
+            // Each number multiplied by 3
+            // take all number, but 9
+            // Order number by ascending value
+
+            var processedNumberList = 
+                numbers
+                .Select(num => num * 3) // {3, 6, 9 }
+                .Where(num => num != 9 ) // all but the 9
+                .OrderBy(num => num); // at the end, we order ascending
+        }
+        static public void SearchExamples()
+        {
+            List<string> textList = new List<string>()
+            {
+                "a",
+                "b",
+                "c",
+                "c",
+                "d",
+                "e",
+                "f",
+                "g"
+            };
+
+            // First of all element
+            var first = textList.First();
+
+            // First element that in "c"
+            var cText = textList.First(text => text.Equals("c"));
+
+            // First element that contains "j"
+            var jText = textList.First(text => text.Contains("j"));
+
+            //First element that contains "z"
+            var firstOrDefaultText = textList.FirstOrDefault(text => text.Contains("z"));
+
+            //Last element that contains "z"
+            var lastOrDefaultText = textList.LastOrDefault(text => text.Contains("z"));
+
+            // Singlevalue
+            var uniqueTexts = textList.Single();
+            var uniqueOrDefaultTexts = textList.SingleOrDefault();
+
+            int[] eveNumber = { 0, 2, 4, 6, 8 };
+            int[] otherEvenNumber = { 0, 2, 6 };
+
+            // obtain( 4, 8)
+            var myEventNumbers = eveNumber.Except(otherEvenNumber);
+
+        }
+        static public void MultipleSelects()
+        {
+            // Select many
+
+            string[] myOpinions =
+            {
+                "Opinion 1, Text 1",
+                "Opinion 2, Text 2",
+                "Opinion 3, Text 3"
+            };
+
+            var myOpinionSelection = myOpinions.SelectMany(opinion => opinion.Split(","));
+            var enterprises = new[]
+            {
+                new Enterprise()
+                {
+                    Id = 1,
+                    Name = "Enterprise 1",
+                    Employees = new[]
+                    {
+                        new Employee
+                        {
+                            Id = 1,
+                            Name = "Mateo",
+                            Email = "mateo@example.com",
+                            Salary = 3000
+                        },
+                        new Employee
+                        {
+                            Id = 1,
+                            Name = "Juan",
+                            Email = "juan@example.com",
+                            Salary = 1000
+                        },
+                        new Employee
+                        {
+                            Id = 1,
+                            Name = "Marcos",
+                            Email = "marcos@example.com",
+                            Salary = 2000
+                        }
+                    }
+                },
+                new Enterprise()
+                {
+                    Id = 2,
+                    Name = "Enterprise 2",
+                    Employees = new[]
+                    {
+                        new Employee
+                        {
+                            Id = 4,
+                            Name = "Maria",
+                            Email = "maria@example.com",
+                            Salary = 3000
+                        },
+                        new Employee
+                        {
+                            Id = 5,
+                            Name = "Martha",
+                            Email = "martha@example.com",
+                            Salary = 1500
+                        },
+                        new Employee
+                        {
+                            Id = 6,
+                            Name = "Elizabeth",
+                            Email = "elizabeth@exmple.com",
+                            Salary = 4000
+                        }
+                    }
+                }
+            };
+
+            // Obtain all employeeList
+            var employeeList = enterprises.SelectMany(enterprise => enterprise.Employees);
+
+            // Know yf any list is empty
+            bool hasEnterprises = enterprises.Any();
+
+            bool hasEmployees = enterprises.Any(enterprise => enterprise.Employees.Any());
+
+            // All enterprises at list has employees with at least Salary 1000
+            bool hasEmployeeWithSalaryMoreThanOrEqual1000 = 
+                enterprises.Any(enterprise => 
+                enterprise.Employees.Any(employee => 
+                employee.Salary >= 1000));
+        }
+
+        static public void LinqCollections()
+        {
+            var firstList = new List<string> { "a", "b", "c" };
+            var secondList = new List<string> { "a", "c", "d" };
+
+            // Inner Join
+            var commonResult = from element in firstList
+                               join secondElement in secondList
+                               on element equals secondElement
+                               select new { element, secondElement };
+
+            var commonResult2 = firstList.Join(
+                    secondList,
+                    element => element,
+                    secondElement => secondElement,
+                    (element, secondElement) => new { element, secondElement }
+                );
+
+            // Outer Join - Left
+            var leftOuterJoin = from element in firstList
+                                join secondElemnt in secondList
+                                on element equals secondElemnt
+                                into temporalList
+                                from temporalElemnt in temporalList.DefaultIfEmpty()
+                                where element != temporalElemnt
+                                select new { Element = element };
+
+            var leftOuterJoin2 = from element in firstList
+                                 from secondElement in secondList.Where(s => s == element).DefaultIfEmpty()
+                                 select new { Element = element, SecondElement = secondElement };
+
+            //Outer join -right
+            var rightOuterJoin = from secondElement in secondList
+                                join element in firstList
+                                on secondElement equals element
+                                into temporalList
+                                from temporalElemnt in temporalList.DefaultIfEmpty()
+                                where secondElement != temporalElemnt
+                                select new { Element = secondElement };
+
+            var RightOuterJoin2 = from element in firstList
+                                 from secondElement in secondList.Where(s => s == element).DefaultIfEmpty()
+                                 select new { Element = element, SecondElement = secondElement };
+
+            // Union
+            var unionList = leftOuterJoin.Union(rightOuterJoin);
+
+        }
+
+        static public void SkipTakeLink()
+        {
+            var myList = new[]
+            {
+                1,2,3,4,5,6,7,8,9,10
+            };
+
+            //SKIP
+            var skipTwoFirstValues = myList.Skip(2);
+            var skipLastTwoValues = myList.SkipLast(2);
+            var skipWhileSmallerThan4 = myList.SkipWhile(s => s < 4);
+
+            // TAKE
+            var takeFirstTwoValues = myList.Take(2);
+            var takeLastTwoValues = myList.TakeLast(2);
+            var takeWhileSmallerThan4 = myList.TakeWhile(s => s < 4);
+
+        }
+    }
+}
---
+++ University/Unversity/UnversityApiBackend/Services/Service.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Configuration;
+using UniversityApiBackend.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models.DataModels;
+
+namespace LinqSnippets
+{
+    public class Service
+    {
+        //Buscar usuarios por email
+
+        static public void findUserByEmail(string email)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var connectionString = configuration.GetSection("ConnectionStrings")["UniversityDB"];
+
+            using (var context = new UniversityDBContext(new DbContextOptionsBuilder<UniversityDBContext>()
+                .UseSqlServer(connectionString)
+                .Options))
+            {
+                var user = context.Users.FirstOrDefault(u => u.EmailAddress == email);
+                if (user != null)
+                {
+                    // El usuario fue encontrado
+                }
+                else
+                {
+                    // El usuario no fue encontrado
+                }
+            }
+        }
+
+        //Buscar alumnos mayores de edad
+        static public List<Student> GetMinorStudents()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var connectionString = configuration.GetSection("ConnectionStrings")["UniversityDB"];
+
+            using (var context = new UniversityDBContext(new DbContextOptionsBuilder<UniversityDBContext>()
+                .UseSqlServer(connectionString)
+                .Options))
+            {
+                var minorStudents = context.Students.Where(s => DateTime.Today.Year - s.Dob.Year < 18).ToList();
+                return minorStudents;
+            }
+        }
+
+        //Buscar alumnos que tengan al menos un curso
+        static public List<Student> GetStudentsWithCourse()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var connectionString = configuration.GetSection("ConnectionStrings")["UniversityDB"];
+
+            using (var context = new UniversityDBContext(new DbContextOptionsBuilder<UniversityDBContext>()
+                .UseSqlServer(connectionString)
+                .Options))
+            {
+                var studentsWithCourse = context.Students.Include(s => s.Courses).Where(s => s.Courses.Count > 0).ToList();
+                return studentsWithCourse;
+            }
+        }
+
+        //Buscar cursos de un nivel determinado que al menos tengan un alumno inscrito
+        static public List<Course> FindCoursesByLevelWithStudents(Level level)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var connectionString = configuration.GetSection("ConnectionStrings")["UniversityDB"];
+
+            using (var context = new UniversityDBContext(new DbContextOptionsBuilder<UniversityDBContext>()
+                .UseSqlServer(connectionString)
+                .Options))
+            {
+                var courses = context.Courses
+                    .Include(c => c.Students)
+                    .Where(c => c.Level == level && c.Students.Any())
+                    .ToList();
+
+                return courses;
+            }
+        }
+
+
+        //Buscar cursos de un nivel determinado que sean de una categoría determinada
+        static public List<Course> FindCoursesByLevelAndCategory(Level level, string categoryName)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var connectionString = configuration.GetSection("ConnectionStrings")["UniversityDB"];
+
+            using (var context = new UniversityDBContext(new DbContextOptionsBuilder<UniversityDBContext>()
+                .UseSqlServer(connectionString)
+                .Options))
+            {
+                var courses = context.Courses
+                    .Include(c => c.Categories)
+                    .Where(c => c.Level == level && c.Categories.Any(cat => cat.Name == categoryName))
+                    .ToList();
+
+                return courses;
+            }
+        }
+
+
+        //Buscar cursos sin alumnos
+        static public List<Course> GetCoursesWithoutStudents()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var connectionString = configuration.GetSection("ConnectionStrings")["UniversityDB"];
+
+            using (var context = new UniversityDBContext(new DbContextOptionsBuilder<UniversityDBContext>()
+                .UseSqlServer(connectionString)
+                .Options))
+            {
+                var courses = context.Courses.Where(c => c.Students.Count == 0).ToList();
+                return courses;
+            }
+        }
+    }
+}
